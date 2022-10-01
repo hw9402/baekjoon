54a31fe6cad1e30758d43e476facdccb92d17833
hw9402 <mhw35689402@gmail.com>
2022-10-02 00:18:34 +0900

    25083

---
+++ 1. 입출력과 사칙연산/25083.cpp
@@ -0,0 +1,22 @@
+/*
+ 문제
+ 아래 예제와 같이 새싹을 출력하시오.
+
+ 입력
+ 없음.
+
+ 출력
+*/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+int main() {
+    cout << "         ,r'\"7" << "\n";
+    cout << "r`-_   ,'  ,/" << "\n";
+    cout << " \\. \". L_r'" << "\n";
+    cout << "   `~\\/" << "\n";
+    cout << "      |" << "\n";
+    cout << "      |" << "\n";
+    return 0;
+}
