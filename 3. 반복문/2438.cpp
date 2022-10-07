8f64faa8cc5d6d8ac85d4fff374da376baf18704
hw9402 <mhw35689402@gmail.com>
2022-10-08 00:54:19 +0900

    solved 2438

---
+++ 3. 반복문/2438.cpp
@@ -0,0 +1,24 @@
+/*
+문제
+첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+
+입력
+첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+
+출력
+첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+*/
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n, i, j;
+    cin >> n;
+    for(i=0; i<n; i++) {
+        for(j=0; j<=i; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
